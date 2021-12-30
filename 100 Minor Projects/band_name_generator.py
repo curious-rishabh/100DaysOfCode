2e329697f1509cb7a0ec769b085ea447fa12daa1
Rishabh Baghel <92927022+curiousrishabh@users.noreply.github.com>
2021-12-31 00:51:13 +0530

    Delete 100 Minor Projects directory

--- 100 Minor Projects/band_name_generator.py
+++
@@ -1,6 +0,0 @@
-# Band Name Generator
-# Day 1 of 100DaysOfCode
-print("Welcome to Band Name Generator.")
-city = input("which city did you grow up in?\n")
-pet = input("What is name of your pet?\n")
-print("Yout band name could be " + city + " " + pet)
